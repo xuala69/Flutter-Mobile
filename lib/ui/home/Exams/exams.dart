0186c815e99a0f949f2b2bdc16240478a567416d
Lawma <zuala@lailen.com>
2024-05-28 22:20:50 +0530

    exams list changed to grid

--- lib/ui/home/Exams/exams.dart
+++ lib/ui/home/Exams/exams.dart
@@ -1,11 +1,14 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:prep_pro/controllers/categories_controller.dart';
 import 'package:prep_pro/models/exams.dart';
 import 'package:prep_pro/ui/home/Filter/filter_sheet.dart';
 import 'package:prep_pro/ui/widgets/spacing.dart';
 import 'package:prep_pro/utils/nums.dart';
+import 'package:prep_pro/utils/strings.dart';
 import 'exams_ui_controller.dart';
 import 'widgets/searchbar.dart';
 
@@ -48,64 +51,121 @@ class _HomeExamsPageState extends State<HomeExamsPage> {
             onRefresh: () => Future.sync(
               () => uiCtrl.pagingController.refresh(),
             ),
-            child: PagedListView.separated(
-                builderDelegate: PagedChildBuilderDelegate<Exam>(
-                  itemBuilder: (context, item, index) {
-                    return Card(
-                      child: ListTile(
-                        title: Text(item.name),
-                        subtitle: Text(item.organization?.name ?? ""),
+            child: PagedGridView(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisExtent: 220,
+                mainAxisSpacing: Nums.paddingSmall,
+                crossAxisSpacing: Nums.paddingSmall,
+              ),
+              builderDelegate: PagedChildBuilderDelegate<Exam>(
+                itemBuilder: (context, item, index) {
+                  return GridTile(
+                    footer: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 5),
+                      decoration: BoxDecoration(
+                          color: Colors.black54,
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(
+                              Nums.searchbarRadius,
+                            ),
+                            bottomRight: Radius.circular(
+                              Nums.searchbarRadius,
+                            ),
+                          )),
+                      child: Text(
+                        item.name,
+                        style: GoogleFonts.spectral(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.center,
                       ),
-                    );
-                  },
-                  firstPageErrorIndicatorBuilder: (context) => hs(0),
-                  noItemsFoundIndicatorBuilder: (context) => SizedBox(
-                    height: Get.height * 0.6,
-                    child: Center(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          SizedBox(
-                              height: Get.height * 0.20,
-                              child: Image.asset(
-                                "assets/empty_songs.png",
-                                opacity:
-                                    const AlwaysStoppedAnimation<double>(0.6),
-                              )),
-                          vs(35),
-                          Text(
-                            "No items found",
-                            style:
-                                Theme.of(context).textTheme.bodyLarge!.copyWith(
-                                      fontSize: 22,
-                                    ),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Colors.grey[300]!,
                           ),
-                          vs(10),
-                          Text(
-                            "Please try again later",
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyLarge!
-                                .copyWith(
-                                    fontSize: 16,
-                                    color: Theme.of(context)
-                                        .textTheme
-                                        .bodyLarge!
-                                        .color!
-                                        .withOpacity(0.8)),
+                          top: BorderSide(
+                            color: Colors.grey[300]!,
                           ),
-                        ],
+                          left: BorderSide(
+                            color: Colors.grey[300]!,
+                          ),
+                          right: BorderSide(
+                            color: Colors.grey[300]!,
+                          ),
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            Nums.searchbarRadius,
+                          ),
+                        ),
+                        image: DecorationImage(
+                          image: CachedNetworkImageProvider(
+                            item.imagePath ?? Strings.avatarDefault,
+                          ),
+                        ),
+                        color: Colors.grey[50],
                       ),
+                    ),
+                  );
+                },
+                firstPageErrorIndicatorBuilder: (context) => hs(0),
+                noItemsFoundIndicatorBuilder: (context) => SizedBox(
+                  height: Get.height * 0.6,
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        SizedBox(
+                            height: Get.height * 0.20,
+                            child: Image.asset(
+                              "assets/empty_songs.png",
+                              opacity:
+                                  const AlwaysStoppedAnimation<double>(0.6),
+                            )),
+                        vs(35),
+                        Text(
+                          "No items found",
+                          style:
+                              Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                    fontSize: 22,
+                                  ),
+                        ),
+                        vs(10),
+                        Text(
+                          "Please try again later",
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyLarge!
+                              .copyWith(
+                                  fontSize: 16,
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyLarge!
+                                      .color!
+                                      .withOpacity(0.8)),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-                pagingController: uiCtrl.pagingController,
-                padding: EdgeInsets.only(
-                  left: Nums.paddingNormal,
-                  right: Nums.paddingNormal,
-                  bottom: 110,
-                ),
-                separatorBuilder: (context, index) => vs(5)),
+              ),
+              pagingController: uiCtrl.pagingController,
+              padding: EdgeInsets.only(
+                left: Nums.paddingNormal,
+                right: Nums.paddingNormal,
+                bottom: 110,
+              ),
+              // separatorBuilder: (context, index) => vs(5),
+            ),
           ),
         )
       ],
