de4df8be415bf365f619f42f021a22d351fadb45
Lawma <zuala@lailen.com>
2024-07-17 23:23:08 +0530

    show logo at appbar

--- lib/ui/widgets/m_appbar.dart
+++ lib/ui/widgets/m_appbar.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:prep_pro/ui/home/Profile/profile_main.dart';
 import 'package:prep_pro/ui/widgets/spacing.dart';
 import 'package:prep_pro/utils/colors.dart';
@@ -9,22 +8,16 @@ import 'package:prep_pro/utils/strings.dart';
 
 AppBar mAppbar() {
   return AppBar(
-    title: Row(
-      children: [
-        const Icon(
-          Icons.g_mobiledata,
-          size: 45,
-          color: Colors.white,
+    title: Container(
+      height: 40,
+      width: 40,
+      // margin: const EdgeInsets.all(5),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/logo.jpeg"),
         ),
-        Text(
-          "1% Club",
-          style: GoogleFonts.roboto(
-            color: Colors.grey[200],
-            fontSize: 16,
-            fontWeight: FontWeight.w700,
-          ),
-        )
-      ],
+        shape: BoxShape.circle,
+      ),
     ),
     centerTitle: false,
     backgroundColor: AppColors.primary,
